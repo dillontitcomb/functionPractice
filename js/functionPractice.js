9b5eca8bbfc7e2700d67b11479c02f0b016594e4
Alexander Neumann and Dillon Titcomb <dillon@>
2018-01-08 10:39:20 -0800

    Added fahrenheit to Celcius, Celcius to Kelvin, Fahrenheit to Kelvin, and BMI calculator

--- js/functionPractice.js
+++ js/functionPractice.js
@@ -3,13 +3,62 @@ var yourName = function() {
   alert("Your name is " + myPromptInput);
 }
 
+var sentence = function() {
+  var yourName = prompt("What is your name?");
+  var yourAge = prompt("How old are you?");
+  var yourFood = prompt("What is your favorite food?");
+  alert("Your name is " + yourName + ", your age is " + yourAge + ", and your favorite food is " + yourFood + ".");
+}
 
 var add = function(number1, number2) {
   return number1 + number2;
 }
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+var multiplyThree = function(number1, number2, number3) {
+  return number1 * number2 * number3;
+}
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+
+var modulus = function(number1, number2) {
+  return number1 % number2;
+}
+var fToc = function(number1) {
+  return (9/5 * number1) + 32;
+}
+
+var cToK = function(number1) {
+  return (number1 + 273.15);
+}
+
+var fToK = function(number1) {
+  return cToK(fToc(number1));
+}
+
+var weightToBMI = function() {
+  var sex = prompt("What is your sex?");
+  var weight = prompt("What is your weight?");
+  var height = prompt("What is your height");
+  if (sex===true) {
+    return ((1.10*weight) - 128((weight*weight)/(100*(height*height))));
+  } else {
+    return ((1.07*weight) - 128((weight*weight)/(100*(height*height))));
+  }
+  return "PLEASE";
+}
+
 $(function(){
   $("h1").click(function() {
-    yourName();
+    sentence();
   });
 });
